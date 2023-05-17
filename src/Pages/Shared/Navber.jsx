31f24945d9a96fcefa9c1fe0053c77a4b254b83f
golam-kibriya123 <golamkibriya622@gmail.com>
2023-05-17 07:16:50 -0500

    added banner

--- src/Pages/Shared/Navber.jsx
+++ src/Pages/Shared/Navber.jsx
@@ -2,6 +2,10 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg'
 
 const Navber = () => {
+    const NavItems = <>
+        <li><Link to={'/'} href="">Home</Link ></li>
+        <li><Link to={'/about'} href="">About</Link ></li>
+    </>
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
@@ -10,40 +14,19 @@ const Navber = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <a className="justify-between">
-                                Parent
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                            </a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {NavItems}
                     </ul>
                 </div>
                 <Link to={'/'} className=""><img src={logo} className='h-20 w-28' /></Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
-                        <a>
-                            Parent
-                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                        </a>
-                        <ul className="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {NavItems}
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Get started</a>
+            <button className="btn btn-outline btn-warning">Appointment</button>
+
             </div>
         </div>
     );
